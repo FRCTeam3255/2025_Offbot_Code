751b17ab248d94c6da2b9e07963d09f2adc07f08
BrodyKarr <145169276+BrodyKarr@users.noreply.github.com>
2025-07-20 15:54:23 -0700

    created the statemachine
    I made the "shell" of the statemachine based of the given list for the states, and our drive states from on season.

--- src/main/java/frc/robot/subsystems/StateMachine.java
+++ src/main/java/frc/robot/subsystems/StateMachine.java
@@ -21,12 +21,22 @@ public class StateMachine extends SubsystemBase {
   @NotLogged
   Drivetrain subDrivetrain;
   @NotLogged
+  Elevator subElevator;
+  @NotLogged
+  Intake subIntake;
+  @NotLogged
+  Climber subClimber;
+  @NotLogged
   StateMachine subStateMachine = this;
 
   /** Creates a new StateMachine. */
-  public StateMachine(Drivetrain subDrivetrain) {
+  public StateMachine(Drivetrain subDrivetrain, Elevator subElevator, Intake subIntake,
+      Climber subClimber) {
     currentRobotState = RobotState.NONE;
     currentDriverState = DriverState.MANUAL;
+    this.subElevator = subElevator;
+    this.subIntake = subIntake;
+    this.subClimber = subClimber;
 
     this.subDrivetrain = subDrivetrain;
   }
@@ -54,21 +64,354 @@ public class StateMachine extends SubsystemBase {
           case NONE:
             return new None(subStateMachine);
         }
+      
         break;
-    }
-    return Commands
-        .print("ITS SO OVER D: Invalid State Provided, Blame Eli. Attempted to go to: " + desiredState.toString()
-            + " while at " + currentRobotState.toString());
-  }
+
+// climbing states
+
+      case PREP_CLIMB:
+        switch (currentRobotState) {
+          
+            return new PrepClimb(subStateMachine);
+          
+        }
+        break;
+
+      case CLIMBING:
+        switch (currentRobotState) {
+          
+            return new Climbing(subStateMachine);
+          
+        }
+        break;
+
+// Prep Coral only        
+      case PREP_CORAL_ZERO:   
+        switch (currentRobotState) {
+  
+    return new PrepCoralZero(subStateMachine);
+  
+}
+break;
+
+case PREP_CORAL_L1:   
+        switch (currentRobotState) {
+  
+    return new PrepCoralLv(subStateMachine);
+  
+}
+break;
+
+case PREP_CORAL_L2:   
+        switch (currentRobotState) {
+  
+    return new PrepCoralLv(subStateMachine);
+  
+}
+break;
+
+case PREP_CORAL_L3:   
+        switch (currentRobotState) {
+  
+    return new PrepCoralLv(subStateMachine);
+  
+}
+break;
+
+case PREP_CORAL_L4:   
+        switch (currentRobotState) {
+  
+    return new PrepCoralLv(subStateMachine);
+  
+}
+break;
+
+// prep Coral with Algae
+      
+case PREP_CORAL_WITH_ALGAE_L1:   
+        switch (currentRobotState) {
+  
+    return new PrepCoralWithAlgae(subStateMachine);
+  
+}
+break;
+
+case PREP_CORAL_WITH_ALGAE_L2:   
+        switch (currentRobotState) {
+  
+    return new PrepCoralWithAlgae(subStateMachine);
+  
+}
+break;
+
+case PREP_CORAL_WITH_ALGAE_L3:   
+        switch (currentRobotState) {
+  
+    return new PrepCoralWithAlgae(subStateMachine);
+  
+}
+break;
+
+case PREP_CORAL_WITH_ALGAE_L4:   
+        switch (currentRobotState) {
+  
+    return new PrepCoralWithAlgae(subStateMachine);
+  
+}
+break;
+
+case PREP_CORAL_ZERO_WITH_ALGAE:   
+        switch (currentRobotState) {
+  
+    return new PrepCoralWithAlgae(subStateMachine);
+  
+}
+break;
+
+// prep Algae only  
+
+case PREP_ALGAE_NET:   
+        switch (currentRobotState) {
+  
+    return new PrepNet(subStateMachine);
+  
+}
+break;
+
+case PREP_ALGAE_PROCESSOR:   
+        switch (currentRobotState) {
+  
+    return new PrepProcessor(subStateMachine);
+  
+}
+break;
+
+case PREP_ALGAE_ZERO:   
+        switch (currentRobotState) {
+  
+    return new PrepZero(subStateMachine);
+  
+}
+break;
+
+// prep Algae with Coral
+
+case PREP_ALGAE_NET_WITH_CORAL:   
+        switch (currentRobotState) {
+  
+    return new PrepNetWithCoral(subStateMachine);
+  
+}
+break;
+
+case PREP_ALGAE_PROCESSOR_WITH_CORAL:   
+        switch (currentRobotState) {
+  
+    return new PrepProcessorWithCoral(subStateMachine);
+  
+}
+break;
+
+case PREP_ALGAE_ZERO_WITH_CORAL:   
+        switch (currentRobotState) {
+  
+    return new PrepZeroWithCoral(subStateMachine);
+  
+}
+break;
+
+// holding 1 game piece
+
+      case HAS_CORAL:
+        switch (currentRobotState) {
+          
+            return new HasCoral(subStateMachine);
+        }
+        break;
+
+      case HAS_ALGAE:
+        switch (currentRobotState) {
+         
+            return new HasAlgae(subStateMachine);
+        }
+        break;
+
+// holding 2 game pieces
+      case HAS_CORAL_AND_ALGAE:
+        switch (currentRobotState) {
+          
+            return new HasCoralAndAlgae(subStateMachine);
+        }
+        break;
+
+// manipulating 1 game piece
+
+
+      case SCORING_CORAL:
+        switch (currentRobotState) {
+          
+            return new ScoringCoral(subStateMachine);
+        }
+        break;
+
+      case SCORING_ALGAE:
+        switch (currentRobotState) {
+          
+            return new ScoringAlgae(subStateMachine);
+        }
+        break;
+
+      case CLEAN_HIGH:
+        switch (currentRobotState) {
+          
+            return new CleanHigh(subStateMachine);
+        }
+        break;
+
+      case CLEAN_LOW:
+        switch (currentRobotState) {
+          
+            return new CleanLow(subStateMachine);
+        }
+        break;
+
+      case INTAKE_CORAL_STATION:
+        switch (currentRobotState) {
+          
+            return new IntakeCoralStation(subStateMachine);
+        }
+        break;
+
+      case INTAKE_ALGAE_GROUND:
+        switch (currentRobotState) {
+          
+            return new IntakeAlgaeGround(subStateMachine);
+        }
+        break;
+
+// manipulating 2 game pieces
+      case EJECTING:
+        switch (currentRobotState) {
+          
+            return new Ejecting(subStateMachine);
+        }
+        break;
+
+      case SCORING_ALGAE_WITH_CORAL:
+        switch (currentRobotState) {
+          
+            return new ScoringAlgaeWithCoral(subStateMachine);
+        }
+        break;
+
+      case SCORING_CORAL_WITH_ALGAE:
+        switch (currentRobotState) {
+          
+            return new ScoringCoralWithAlgae(subStateMachine);
+        }
+        break;
+
+      case CLEAN_HIGH_WITH_CORAL:
+        switch (currentRobotState) {
+          
+            return new CleanHighWithCoral(subStateMachine);
+        }
+        break;
+
+      case CLEAN_LOW_WITH_CORAL:
+        switch (currentRobotState) {
+          
+            return new CleanLowWithCoral(subStateMachine);
+        }
+        break;
+
+      case INTAKE_CORAL_GROUND:
+        switch (currentRobotState) {
+          
+            return new IntakeCoralGround(subStateMachine);
+        }
+        break;
+
+      case INTAKE_CORAL_WITH_ALGAE_GROUND:
+        switch (currentRobotState) {
+          
+            return new IntakeCoralWithAlgaeGround(subStateMachine);
+        }
+        break;
+
+      case INTAKE_ALGAE_WITH_CORAL_GROUND:
+        switch (currentRobotState) {
+          
+            return new IntakeAlgaeWithCoralGround(subStateMachine);
+        }
+        break;
+
+    }return Commands.print("ITS SO OVER D: Invalid State Provided, Blame Eli. Attempted to go to: "+desiredState.toString()+" while at "+currentRobotState.toString());
+}
 
   public enum DriverState {
-    MANUAL
+    MANUAL,
+    REEF_ROTATION_SNAPPING,
+    CORAL_STATION_ROTATION_SNAPPING,
+    REEF_AUTO_DRIVING,
+    CORAL_STATION_AUTO_DRIVING,
+    PROCESSOR_ROTATION_SNAPPING,
+    PROCESSOR_AUTO_DRIVING,
+    NET_ROTATION_SNAPPING,
+    NET_AUTO_DRIVING,
+    ALGAE_ROTATION_SNAPPING,
+    ALGAE_AUTO_DRIVING,
+    CAGE_ROTATION_SNAPPING
     // TODO: Add other driver states as needed
   }
 
   public enum RobotState {
-    NONE
-    // TODO: Add other robot states as needed
+    NONE,
+    // climbing states
+    PREP_CLIMB,
+    CLIMBING,
+    // Prep Coral only
+    PREP_CORAL_ZERO,
+    PREP_CORAL_L1,
+    PREP_CORAL_L2,
+    PREP_CORAL_L3,
+    PREP_CORAL_L4,
+    // prep Coral with Algae
+    PREP_CORAL_WITH_ALGAE_L1,
+    PREP_CORAL_WITH_ALGAE_L2,
+    PREP_CORAL_WITH_ALGAE_L3,
+    PREP_CORAL_WITH_ALGAE_L4,
+    PREP_CORAL_ZERO_WITH_ALGAE,
+    // prep Algae only
+    PREP_ALGAE_NET,
+    PREP_ALGAE_PROCESSOR,
+    PREP_ALGAE_ZERO,
+    // prep Algae with Coral
+    PREP_ALGAE_NET_WITH_CORAL,
+    PREP_ALGAE_PROCESSOR_WITH_CORAL,
+    PREP_ALGAE_ZERO_WITH_CORAL,
+    // holding 1 game piece
+    HAS_CORAL,
+    HAS_ALGAE,
+    // holding 2 game pieces
+    HAS_CORAL_AND_ALGAE,
+    // manipulating 1 game piece
+    SCORING_CORAL,
+    SCORING_ALGAE,
+    CLEAN_HIGH,
+    CLEAN_LOW,
+    INTAKE_CORAL_STATION,
+    INTAKE_ALGAE_GROUND,
+    // manipulating 2 game pieces
+    EJECTING, // we are planning on ejecting both game pieces at the same time
+    SCORING_ALGAE_WITH_CORAL,
+    SCORING_CORAL_WITH_ALGAE,
+    CLEAN_HIGH_WITH_CORAL,
+    CLEAN_LOW_WITH_CORAL,
+    INTAKE_CORAL_GROUND,
+    INTAKE_CORAL_WITH_ALGAE_GROUND,
+    INTAKE_ALGAE_WITH_CORAL_GROUND,
+
   }
 
   @Override
