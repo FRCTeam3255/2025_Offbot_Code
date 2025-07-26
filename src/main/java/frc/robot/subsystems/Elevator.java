b68b50a9ac8a7cb428e1973dc8837110473c0db6
FRCTeam3255-Shared <170778697+FRCTeam3255-Shared@users.noreply.github.com>
2025-07-26 13:41:02 -0700

    Started machanism 2d ex

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -8,12 +8,17 @@ import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.RobotMap.mapElevator;
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Elevator extends SubsystemBase {
   TalonFX elevatorLeftMotor;
   TalonFX elevatorRightMotor;
   TalonFX elevatorLeftPivotMotor;
   TalonFX elevatorRightPivotMotor;
+  MechanismLigament2d elevator;
 
   /** Creates a new Elevator. */
   public Elevator() {
@@ -23,12 +28,20 @@ public class Elevator extends SubsystemBase {
     elevatorLeftPivotMotor = new TalonFX(mapElevator.ELEVATOR_LEFT_PIVOT_CAN); // Elevator left pivot motor
     elevatorRightPivotMotor = new TalonFX(mapElevator.ELEVATOR_RIGHT_PIVOT_CAN); // Elevator right pivot motor
 
+    // the main mechanism object
+    Mechanism2d mech = new Mechanism2d(3, 3);
+    // the mechanism root node
+    MechanismRoot2d root = mech.getRoot("climber", 2, 0);
+    elevator = root.append(new MechanismLigament2d("elevator", .5, 90));
     // Set default motor configurations if needed
     // e.g., elevatorLeftMotor.configFactoryDefault();
+    // post the mechanism to the dashboard
+    SmartDashboard.putData("Mech2d", mech);
   }
 
   @Override
   public void periodic() {
+    elevator.setLength(2);
     // This method will be called once per scheduler run
   }
 }
