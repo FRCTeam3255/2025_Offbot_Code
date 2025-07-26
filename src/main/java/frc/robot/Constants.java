ddadbdffa99bf121a016fd48f7f98c072afb2572
BrodyKarr <145169276+BrodyKarr@users.noreply.github.com>
2025-07-25 18:31:26 -0700

    made vision based off of on season, and reviewed to  ensure understanding of it🦟™️ (#17)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -247,21 +247,69 @@ public final class Constants {
   }
 
   public static class constVision {
+    public static final String[] LIMELIGHT_NAMES = new String[] { "limelight-right", "limelight-left",
+        "limelight-back" };
+
     /**
      * <p>
      * Pose estimator standard deviation for vision data
      * <p>
      * <b>Units:</b> Meters
      */
-    public static final double STD_DEVS_POS = 0.7;
 
+    public static final double STD_DEVS_POS = 0.7;
     /**
      * <p>
      * Pose estimator standard deviation for vision data
      * </p>
      * <b>Units:</b> Radians
      */
+
     public static final double STD_DEVS_HEADING = 9999999;
 
+    /**
+     * <p>
+     * Pose estimator standard deviation for vision data
+     * </p>
+     * <b>Units:</b> Meters
+     */
+    public static final double MEGA_TAG1_STD_DEVS_POSITION = .3;
+
+    public static final double MEGA_TAG1_STD_DEVS_HEADING = .1;
+    /**
+     * <p>
+     * Maximum rate of rotation before we begin rejecting pose updates
+     * </p>
+     */
+    public static final AngularVelocity MAX_ANGULAR_VELOCITY = Units.DegreesPerSecond.of(720);
+
+    /**
+     * The area that one tag (if its the only tag in the update) needs to exceed
+     * before being accepted
+     */
+    public static final double AREA_THRESHOLD_FRONT = 0.1;
+    public static final double AREA_THRESHOLD_BACK = 0.05;
+
+    // The below values are accounted for in the limelight interface, NOT in code
+    public static class LIMELIGHT_RIGHT {
+      public static final Distance LL_FORWARD = Units.Meters.of(0.269494);
+      public static final Distance LL_RIGHT = Units.Meters.of(0.307594);
+      public static final Distance LL_UP = Units.Meters.of(0.211328);
+
+      public static final Angle LL_ROLL = Units.Degrees.of(180);
+      public static final Angle LL_PITCH = Units.Degrees.of(23.17);
+      public static final Angle LL_YAW = Units.Degrees.of(51.25);
+    }
+
+    public static class LIMELIGHT_LEFT {
+      public static final Distance LL_FORWARD = Units.Meters.of(0.269494);
+      public static final Distance LL_RIGHT = Units.Meters.of(-0.307594);
+      public static final Distance LL_UP = Units.Meters.of(0.211328);
+
+      public static final Angle LL_ROLL = Units.Degrees.of(180);
+      public static final Angle LL_PITCH = Units.Degrees.of(23.17);
+      public static final Angle LL_YAW = Units.Degrees.of(-51.25);
+
+    }
   }
 }
