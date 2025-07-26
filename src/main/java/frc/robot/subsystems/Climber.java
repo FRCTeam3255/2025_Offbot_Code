ac9edbdaa468669035b9a37d575d4fc0a581c8f5
BrodyKarr <145169276+BrodyKarr@users.noreply.github.com>
2025-07-26 14:32:10 -0700

    updated the climber subsystem

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -4,19 +4,32 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.controls.NeutralOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.constClimber;
 import frc.robot.RobotMap.mapClimber;
 
 public class Climber extends SubsystemBase {
-  TalonFX climberLeftMotor;
+  TalonFX cageCollectMotor;
 
   /** Creates a new Climber. */
   public Climber() {
-    climberLeftMotor = new TalonFX(mapClimber.CLIMBER_LEFT_CAN); // Climber left motor, using CAN ID 31 as per RobotMap
+    cageCollectMotor = new TalonFX(mapClimber.CAGE_COLLECTER_CAN); // Climber left motor, using CAN ID 31 as per
+
+    cageCollectMotor.getConfigurator().apply(constClimber.CLIMBER_CONFIG);
+    // RobotMap
     // Set default motor configurations if needed
     // e.g., climberLeftMotor.configFactoryDefault();
+  }
+
+  public void setClimberMotorPercentOutput(double speed) {
+    cageCollectMotor.set(speed);
+  }
+
+  public void setClimberNuetralOutput() {
+    cageCollectMotor.setControl(new NeutralOut());
   }
 
   @Override
