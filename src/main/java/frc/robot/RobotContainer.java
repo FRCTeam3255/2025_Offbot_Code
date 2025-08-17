171ed632f5a32b6a4e2501a041ec3d3c801918cd
Tej shah <tejshahred@gmail.com>
2025-08-16 17:16:13 -0700

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -121,7 +121,7 @@ public class RobotContainer {
     subDrivetrain
         .setDefaultCommand(
             new DriveManual(subDrivetrain, subStateMachine, conDriver.axis_LeftY, conDriver.axis_LeftX,
-                conDriver.axis_RightX));
+                conDriver.axis_RightX, subMotion));
 
     configDriverBindings();
     configOperatorBindings();
