8ce92b6c8dc470a908ea113090714e50882b50dd
Tayler Uva <8679670+TaylerUva@users.noreply.github.com>
2025-10-04 15:10:39 -0700

    attempt vision fix

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -4,15 +4,15 @@
 
 package frc.robot.subsystems;
 
+import java.util.Optional;
+
 import com.frcteam3255.utils.LimelightHelpers;
 import com.frcteam3255.utils.LimelightHelpers.PoseEstimate;
-import java.util.Optional;
 
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.epilogue.NotLogged;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.units.measure.AngularVelocity;
-
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.constVision;
 
@@ -165,8 +165,11 @@ public class Vision extends SubsystemBase {
       } else if (lastEstimateLeft.avgTagDist < lastEstimateRight.avgTagDist
           && lastEstimateLeft.avgTagDist < lastEstimateBack.avgTagDist) {
         return Optional.of(lastEstimateLeft);
-      } else {
+      } else if (lastEstimateBack.avgTagDist < lastEstimateRight.avgTagDist
+          && lastEstimateBack.avgTagDist < lastEstimateLeft.avgTagDist) {
         return Optional.of(lastEstimateBack);
+      } else {
+        return Optional.empty();
       }
     }
   }
