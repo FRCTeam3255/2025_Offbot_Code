f7c1a149aed64ead3055b528c5d8f3569d3e2cd4
Evan Grinnell <worldevan8@gmail.com>
2025-10-03 22:26:05 -0700

    Update PID Slot Value (#171)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -487,9 +487,9 @@ public final class Constants {
       CLEAN_HIGH_BACKWARDS.liftHeight = Inches.of(22);
       CLEAN_HIGH_BACKWARDS.pivotAngle = Degrees.of(95.16);
 
-      INTAKE_CORAL_STATION.wristAngle = Degrees.of(10.9);
+      INTAKE_CORAL_STATION.wristAngle = Degrees.of(13.9);
       INTAKE_CORAL_STATION.liftHeight = Inches.of(5.5);// huxly said 6.958
-      INTAKE_CORAL_STATION.pivotAngle = Degrees.of(56.9);
+      INTAKE_CORAL_STATION.pivotAngle = Degrees.of(65.0);
 
       INTAKE_ALGAE_GROUND.wristAngle = Degrees.of(55); // TODO: Replace with actual angle
       INTAKE_ALGAE_GROUND.liftHeight = Inches.of(4); // TODO: Replace with actual height
@@ -518,6 +518,8 @@ public final class Constants {
       PREP_CORAL_L1.wristAngle = Degrees.of(10);
       PREP_CORAL_L1.liftHeight = Inches.of(0); // TODO: Replace with actual height
       PREP_CORAL_L1.pivotAngle = Degrees.of(40);
+      PREP_CORAL_L1.pivotSlot = 1;
+      PREP_CORAL_L1.wristSlot = 1;
 
       PREP_CORAL_L2_FORWARDS.wristAngle = Degrees.of(-115);
       PREP_CORAL_L2_FORWARDS.liftHeight = Inches.of(0); // TODO: Replace with actual height
@@ -594,6 +596,9 @@ public final class Constants {
       PREP_PROCESSOR.wristAngle = Degrees.of(-2); // TODO: Replace with actual angle
       PREP_PROCESSOR.liftHeight = Inches.of(4); // TODO: Replace with actual height
       PREP_PROCESSOR.pivotAngle = Degrees.of(12); // TODO: Replace with actual angle
+      PREP_PROCESSOR.pivotSlot = 1;
+      PREP_PROCESSOR.wristSlot = 0;
+      PREP_PROCESSOR.liftSlot = 0;
 
       CLIMBED.wristAngle = Degrees.of(50); // TODO: Replace with actual angle
       CLIMBED.liftHeight = Inches.of(0); // TODO: Replace with actual height
@@ -626,7 +631,7 @@ public final class Constants {
     public static final double INDEX_L1_SPEED = 0.2;
     public static final double INTAKE_CORAL_ALGAE_SPEED = -1;
     public static final double INTAKE_CORAL_GROUND_SPEED = 1; // TODO: Replace with actual speed
-    public static final double INTAKE_CORAL_STATION_SPEED = 0.7; // TODO: Replace with actual speed
+    public static final double INTAKE_CORAL_STATION_SPEED = 0.6; // TODO: Replace with actual speed
     public static final double SCORE_CORAL_SPEED = 0.4;
     public static final double INTAKE_CORAL_L1_SPEED = 1; // TODO: Replace with actual speed
     public static final double ALGAE_HOLD_SPEED = 0.4;
@@ -736,9 +741,9 @@ public final class Constants {
     public static final Distance FIELD_WIDTH = Units.Feet.of(26).plus(Units.Inches.of(5));
     public static final Pose3d SCORING_ELEMENT_NOT_COLLECTED = new Pose3d(0, 0, -1, Rotation3d.kZero);
     public static final Pose2d RESET_POS = new Pose2d(
-        5.109,
-        5.227,
-        Rotation2d.fromDegrees(-120));
+        3.15,
+        4.0,
+        Rotation2d.fromDegrees(0.0));
 
     public static final Pose2d WORKSHOP_STARTING_POSE = new Pose2d(5.98, 2.60, new Rotation2d(0));
   }
