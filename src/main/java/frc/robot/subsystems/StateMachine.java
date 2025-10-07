9bf3f40b11880bb13fa589e964f6be1627e08341
FRCTeam3255-Shared <eliuva@icloud.com>
2025-10-06 17:16:36 -0700

    Update StateMachine.java

--- src/main/java/frc/robot/subsystems/StateMachine.java
+++ src/main/java/frc/robot/subsystems/StateMachine.java
@@ -78,6 +78,7 @@ public class StateMachine extends SubsystemBase {
           case CLEAN_LOW:
           case EJECTING:
           case INTAKE_CORAL_L1:
+          case SCORING_CORAL_L1:
           case CLIMBING:
             return new None(subStateMachine, subMotion, subRotors);
         }
