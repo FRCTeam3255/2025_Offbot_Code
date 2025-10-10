37326cf12ef431b48707024b0ac2ec68ebd70cf6
Evan Grinnell <worldevan8@gmail.com>
2025-10-09 18:25:27 -0700

    Update L2 Backwards wrist and pivot angles in Constants.java for improved performance

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -533,9 +533,9 @@ public final class Constants {
       PREP_CORAL_L4_FORWARDS.liftHeight = Inches.of(54); // TODO: Replace with actual height
       PREP_CORAL_L4_FORWARDS.pivotAngle = Degrees.of(75);
 
-      PREP_CORAL_L2_BACKWARDS.wristAngle = Degrees.of(-125);
+      PREP_CORAL_L2_BACKWARDS.wristAngle = Degrees.of(-90);
       PREP_CORAL_L2_BACKWARDS.liftHeight = Inches.of(0); // TODO: Replace with actual height
-      PREP_CORAL_L2_BACKWARDS.pivotAngle = Degrees.of(87.64);
+      PREP_CORAL_L2_BACKWARDS.pivotAngle = Degrees.of(110.0);
       PREP_CORAL_L2_BACKWARDS.liftTolerance = Inches.of(5); // TODO: tune tolerances
       PREP_CORAL_L2_BACKWARDS.pivotTolerance = Degrees.of(5); // TODO: tune tolerances
       PREP_CORAL_L2_BACKWARDS.wristTolerance = Degrees.of(5); // TODO: tune tolerances
