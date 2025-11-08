4f694271125f934ffbf6647336afc924bcc58b20
Evan Grinnell <worldevan8@gmail.com>
2025-11-08 11:27:54 -0800

    Made Code Build

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -95,10 +95,6 @@ public class RobotContainer {
       () -> subDriverStateMachine.getDriverState() == DriverStateMachine.DriverState.REEF_AUTO_DRIVING_LEFT);
   private final Trigger isInReefAutoDriveRight = new Trigger(
       () -> subDriverStateMachine.getDriverState() == DriverStateMachine.DriverState.REEF_AUTO_DRIVING_RIGHT);
-  private final Trigger isInReefAutoDriveLeft = new Trigger(
-      () -> subDriverStateMachine.getDriverState() == DriverStateMachine.DriverState.REEF_AUTO_DRIVING_LEFT);
-  private final Trigger isInReefAutoDriveRight = new Trigger(
-      () -> subDriverStateMachine.getDriverState() == DriverStateMachine.DriverState.REEF_AUTO_DRIVING_RIGHT);
 
   Command TRY_NONE = Commands.deferredProxy(
       () -> subStateMachine.tryState(RobotState.NONE));
